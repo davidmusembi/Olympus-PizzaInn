3431c2b7c5b24c936a588ae629e1e2895da9affb
David Musembi <davidmusembi02@gmail.com>
2022-03-19 15:53:52 +0300

    Add a jquery effect to open the hover effect on service page

--- js/script.js
+++ js/script.js
@@ -105,3 +105,22 @@ function clearForm() {
 
   removeFormErrors();
 $(document).ready(function(){
+     //Creating a jquery effect  to open the hover effect on the Service page
+     $(".design").click(function() {
+        $("#design, .design").toggle("slow");
+      });
+      $("#design").click(function() {
+        $(".design, #design").toggle("slow");
+      });
+      $(".development").click(function() {
+        $("#development, .development").toggle("slow");
+      });
+      $("#development").click(function() {
+        $(".development, #development").toggle("slow");
+      });
+      $(".product-mgt").click(function() {
+        $("#product-mgt, .product-mgt").toggle("slow");
+      });
+      $("#product-mgt").click(function() {
+        $(".product-mgt, #product-mgt").toggle("slow");
+      }); 
