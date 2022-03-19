2d58ff1148e1fcab047f39094ee16f1682a6b549
David Musembi <davidmusembi02@gmail.com>
2022-03-19 15:52:27 +0300

    Add a function to validate submit button on contact page

--- js/script.js
+++ js/script.js
@@ -63,3 +63,45 @@ function getSizeCost() {
   
     
   }
+  //Creating a function to validate the submit button on contact page
+var contactForm = $("#contact-form");
+
+contactForm.on("submit", function(e) {
+ e.preventDefault();
+ let formValid = validateContactForm();
+ if (formValid) {
+     const name = $("input.name").val();
+
+     alert(`Thank you ${name}, we have received your message. Thank you for reaching out to us.`);
+     clearForm();
+ }
+});
+function validateContactForm() {
+    let isFormValid = true;
+    const formInputs = contactForm.find("input,textarea");
+
+    formInputs.each(function() {
+        if (!$(this).val()) {
+            $(this).addClass("is-invalid");
+            isFormValid = false;
+        }
+    });
+    return isFormValid;
+}
+
+function removeFormErrors() {
+    contactForm.find("input,textarea").on("keydown", function() {
+        if ($(this).hasClass("is-invalid")) {
+            $(this).removeClass("is-invalid");
+        }
+    });
+}
+
+function clearForm() {
+    contactForm.find("input,textarea").each(function() {
+        $(this).val("");
+    });
+  }
+
+  removeFormErrors();
+$(document).ready(function(){
